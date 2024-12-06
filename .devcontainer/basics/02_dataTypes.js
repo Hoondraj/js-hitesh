93e4013873685ff5623d0633416d498ee25b2d4c
Hoondraj <hoondraj28@gmail.com>
2024-12-06 19:37:36 +0000

    datatype

---
+++ .devcontainer/basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+ //"use strict"; // new js code will be used
+
+//alert ("hitesh")
+
+let name = "Hitesh"
+let age=21
+let isLoggedIn= true
+console.log(name);
+
+// (bigint) used for bigger numbers like in reddit etc
+// (string) 
+// (boolean) yes or no OR true/false
+// (undefined)
+// symbol->unique
+//object
+
+console.log(typeof null);// output is object
+console.log(typeof undefined);// output is undefined
+
